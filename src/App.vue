33e51ece7903f671dbf947595b1f4aa72137e0ad
Valère Pique <valere.pique@sg.social.gouv.fr>
2024-10-16 10:02:47 +0200

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button>dawdawdaw</button>
+    <div style="padding-left: 5rem; padding-right: 5rem">
+      {{ values }}
+      <dsfr-multi-select
+        v-model="values"
+        :options="options"
+        search
+        select-all
+        id-key="key"
+        label-key="value"
+      />
+    </div>
+  </div>
+  <button>dawdawdaw</button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import DsfrMultiSelect from './components/DsfrMultiSelect.vue'
+
+const options = [
+  { key: 'name', value: 'Alice', toto: 'titi' },
+  { key: 'age', value: '25', toto: 'titi' },
+  { key: 'city', value: 'Paris', toto: 'titi' },
+  { key: 'profession', value: 'Engineer', toto: 'titi' },
+  { key: 'hobby', value: 'Photography', toto: 'titi' },
+  { key: 'favorite_color', value: 'Blue', toto: 'titi' },
+]
+const values = ref<string[]>([])
+</script>
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import '@gouvfr/dsfr/dist/dsfr.min.css'
+import '@gouvfr/dsfr/dist/utility/icons/icons.min.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
